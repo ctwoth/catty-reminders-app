b86528a511856e9939f9722b9298223b95b967d7
ctwoth <muromec8906@gmail.com>
2026-05-26 17:32:57 +0400

    Add files via upload

---
+++ run_app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /home/user/catty-reminders-app || exit 1
+
+SHA=$1
+
+echo "Deploying SHA: $SHA"
+git fetch --all
+git reset --hard "$SHA"
+/home/user/catty-reminders-app/venv/bin/python -m pip install -r requirements.txt
+
+echo "DEPLOY_REF=$SHA" | sudo tee /etc/catty-app-env
+sudo systemctl restart catty
+sleep 5
+
+if systemctl is-active --quiet catty; then
+    echo "SUCCESS: SHA: $SHA"
+else
+    echo "ERROR"
+    exit 1
+fi
